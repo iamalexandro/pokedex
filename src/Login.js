5f43f6482961fa9b8243006f94e90981ddd9b32b
Nicola Di Candia <nicoladicandiajaimes@gmail.com>
2023-03-25 15:50:55 -0500

    basic login logic added

---
+++ src/Login.js
@@ -0,0 +1,98 @@
+import React, { useState } from "react";
+import users from "./data/users";
+//libraries
+// import axios from "axios";
+
+//styles
+import "./Login.scss";
+import "../src/index.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+//sources
+import pokedex from "./images/poke.png";
+import pikachu from "./images/pikachu.png";
+
+// pokedex conection
+// const bringPokemons = () => {
+//   axios({
+//     method: "get",
+//     url: "https://pokeapi.co/api/v2/pokemon/ditto",
+//   }).then(function (response) {
+//     console.log(response);
+//   });
+// };
+
+export const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const validaterUser = (username, password) => {
+    const user = users.find((user) => user.username === username);
+    const passwordValidated = users.find((user) => user.password === password);
+
+    if (!user) {
+      console.log("user not found");
+    } else if (user && !passwordValidated) {
+      console.log("password wrong");
+    } else if (user && passwordValidated) {
+      console.log("user logged");
+      const token = user.token;
+      console.log("token: ", token);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    validaterUser(username, password);
+  };
+
+  return (
+    <div className="login">
+      <div className="login-modal">
+        <div className="col-6 login-modal__left center title">
+          <div className="login-modal__form">
+            <span>Sign in</span>
+            <form onSubmit={handleSubmit}>
+              <input
+                type="email"
+                placeholder="youremail@gmail.com"
+                className="login-modal__input mt-5"
+                onChange={(e) => setUsername(e.target.value)}
+                // onChange={(e) => console.log(e.target.value)}
+                value={username}
+              />
+              <input
+                placeholder="Password"
+                type="password"
+                className="login-modal__input mt-4"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+              />
+              <button type="submit" className="login-modal__btn mt-5">
+                SIGN IN
+              </button>
+            </form>
+          </div>
+        </div>
+        <div className="col-6 login-modal__right center text">
+          <div className="login-modal__form title">
+            <span>Hello, Master!</span>
+            <div className="login-modal__subtitle">
+              <p>Are you ready to catch some Pokémons today ?</p>
+            </div>
+          </div>
+          <img className="pikachu" src={pikachu} alt="" width="70%" />
+        </div>
+      </div>
+      <img
+        className="pokedex-logo"
+        src={pokedex}
+        alt="pokedex-logo"
+        width="10%"
+      />
+      <p className="credits">⌨️ con ❤️ por Nicola Di Candia</p>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/data/users.js
@@ -0,0 +1,22 @@
+const users = [
+  {
+    username: "fredymercury@gmail.com",
+    password: "fredthebest",
+    token:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
+  },
+  {
+    username: "johndoe@gmail.com",
+    password: "thedoe123",
+    token:
+      "eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.bQTnz6AuMJvmXXQsVPrxeQNvzDkimo7VNXxHeSBfClLufmCVZRUuyTwJF311JHuh",
+  },
+  {
+    username: "narutohokage@gmail.com",
+    password: "ninetails99",
+    token:
+      "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.NHVaYe26MbtOYhSKkoKYdFVomg4i8ZJd8_-RU8VNbftc4TSMb4bXP3l3YlNWACwyXPGffz5aXHc6lty1Y2t4SWRqGteragsVdZufDn5BlnJl9pdR_kdVFUsra2rWKEofkZeIC4yWytE58sMIihvo9H1ScmmVwBcQP6XETqYd0aSHp1gOa9RdUPDvoXQ5oqygTqVtxaDr6wUFKrKItgBMzWIdNZ6y7O9E0DhEPTbE9rfBo6KTFsHAZnMg4k68CDp2woYIaXbmYTWcvbzIuHO7_37GT79XdIwkm95QJ7hYC9RiwrV7mesbY4PAahERJawntho0my942XheVLmGwLMBkQ",
+  },
+];
+
+export default users;
